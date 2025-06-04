3ee57e56d870c64477391a7d249e6b39140eecab
Abdulloh <abdullohbahromjonv@gmail.com>
2025-06-04 18:24:04 +0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Abdulloh Bahromjonov on 04/06/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
